8b69ef62db8c75f8a7a4b8fc4856dbf925030ab6
SoniIron <soniron032@gmail.com>
2024-12-08 18:51:09 +0530

    Backend Added

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { dbConnection } from "./database/dbConnection.js";
+import messageRouter from "./Routers/messageRouter.js";
+import { errorMiddleware } from "./Middlewares/errorMiddleware.js";
+import userRouter from './Routers/userRouter.js'
+import appointmentRouter from './Routers/appointmentRouter.js'
+
+const app = express();
+configDotenv();
+
+// Middlewares
+app.use(
+  cors({
+    origin: [`${process.env.FRONTEND_URL}`, `${process.env.DASHBOARD_URL}`],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/tmp/"
+}))
+
+
+// Routers Connection
+app.use("/api/v1/message",messageRouter);
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/appointment",appointmentRouter)
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+dbConnection();
+
+// Use in Last to handle all the errors
+app.use(errorMiddleware)
+export default app;
