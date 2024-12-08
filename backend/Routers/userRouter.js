cfa96e082096c3550c9d42cc176138cbfc27e324
SoniIron <soniron032@gmail.com>
2024-12-08 20:02:34 +0530

    Update userRouter.js

--- backend/Routers/userRouter.js
+++ backend/Routers/userRouter.js
@@ -15,7 +15,7 @@ const router = express.Router();
 
 router.post("/patient/register",patientRegister);
 router.post("/login", login);
-router.post("/admin/addnew",addNewAdmin);
+router.post("/admin/addnew", isAdminAuthenticated,addNewAdmin);
 router.get("/doctors", getAllDoctors);
 router.get("/admin/me", isAdminAuthenticated ,getUserDetails);
 router.get("/patient/me", isPatientAuthenticated ,getUserDetails);
